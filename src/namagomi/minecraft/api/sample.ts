eff1cbd07ca44393dbf8b8c9be12a97e5d7d700e
yuuki1293 <yuuki1293@outlook.jp>
2022-05-25 09:10:26 +0900

    fix: json syntax error

--- src/namagomi/minecraft/api/sample.ts
+++ src/namagomi/minecraft/api/sample.ts
@@ -1,13 +1,13 @@
-export const sampleGomiJson = '[\n' +
-    '    {\n' +
-    '        \'name\': \'mekanism\',\n' +
-    '        \'mod-id\': 268560,\n' +
-    '        \'mc-version\': \'1.12.2\'\n' +
-    '    },\n' +
-    '    {\n' +
-    '        \'name\': \'sakura\',\n' +
-    '        \'mod-id\': null,\n' +
-    '        \'mc-version\': \'1.12.2\',\n' +
-    '        \'direct-url\': \'https://github.com/KisaragiEffective/Sakura_mod/releases/download/1.0.8-1.12.2%2Bflavored.ksrg.4/Sakura-1.0.8-1.12.2+flavored.ksrg.4.jar\'\n' +
-    '    }\n' +
-    ']'
+export const sampleGomiJson = "[\n" +
+    "    {\n" +
+    "        \"name\": \"mekanism\",\n" +
+    "        \"mod-id\": 268560,\n" +
+    "        \"mc-version\": \"1.12.2\"\n" +
+    "    },\n" +
+    "    {\n" +
+    "        \"name\": \"sakura\",\n" +
+    "        \"mod-id\": null,\n" +
+    "        \"mc-version\": \"1.12.2\",\n" +
+    "        \"direct-url\": \"https://github.com/KisaragiEffective/Sakura_mod/releases/download/1.0.8-1.12.2%2Bflavored.ksrg.4/Sakura-1.0.8-1.12.2+flavored.ksrg.4.jar\"\n" +
+    "    }\n" +
+    "]"
