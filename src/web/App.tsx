55fc47be558e8788e922f9cd99e1ac45b3ca2548
yuuki1293 <yuuki1293@outlook.jp>
2022-05-26 19:10:37 +0900

    あとでやる

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import {downloadServerModFiles, sampleDownloadServerModFiles} from "../namagomi/minecraft/api/curse_forge";
+import {downloadServerModFiles, sampleDownloadModFiles} from "../namagomi/minecraft/api/curse_forge";
 export const App = () => {
     return (
         <div className="container">
             <div>
-                <button onClick={sampleDownloadServerModFiles}>&#x25BC;</button>
+                <button onClick={sampleDownloadModFiles}>&#x25BC;</button>
                 <button onClick={downloadServerModFiles}>&#x25B2;</button>
-
             </div>
         </div>
     );
