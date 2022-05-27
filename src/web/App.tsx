f72c7db235f19ed44bd4db5727fd7d1910be0695
yuuki1293 <yuuki1293@outlook.jp>
2022-05-27 23:10:47 +0900

    fix: button

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -5,8 +5,8 @@ export const App = () => {
     return (
         <div className="container">
             <div>
-                <button onClick={window.namagomiAPI.sampleDownloadModFilesDev}>&#x25BC;</button>
-                <button onClick={window.namagomiAPI.setupNamagomiLauncherProfile}>&#x25B2;</button>
+                <button onClick={window.namagomiAPI.sampleDownloadModFilesDev}>sampleDownloadModFilesDev</button>
+                <button onClick={window.namagomiAPI.setupNamagomiLauncherProfile}>setupNamagomiLauncherProfile</button>
             </div>
         </div>
     );
