a3d8ccf0471e1c802d74af9c1a1a60276f0c5d93
yuuki1293 <yuuki1293@outlook.jp>
2022-05-28 17:45:18 +0900

    update: local path

--- src/namagomi/settings/localPath.ts
+++ src/namagomi/settings/localPath.ts
@@ -4,7 +4,7 @@ import path from "path";
 export const userDir = app.getPath('userData')
 export const minecraftDir = path.join(userDir, 'minecraft')
 export const devDir = path.join(minecraftDir, 'dev')
-export const mainDir = path.join(userDir, 'main')
+export const mainDir = path.join(minecraftDir, 'main')
 export const devModsDir = path.join(devDir, 'mods')
 export const ModsDir = path.join(mainDir, 'mods')
 export const devConfigDir = path.join(devDir, 'config')
