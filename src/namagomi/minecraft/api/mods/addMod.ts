6199f0cbbd26400285ff3ff8602337361af29cb7
yuuki1293 <yuuki1293@outlook.jp>
2022-05-28 23:20:54 +0900

    add: addMod

--- src/namagomi/minecraft/api/mods/addMod.ts
+++ src/namagomi/minecraft/api/mods/addMod.ts
@@ -1,3 +1,21 @@
-export function addMod(mod: FileList) {
+import {modsDir, namagomiIgnore} from "../../../settings/localPath";
+import fs from "fs";
+import {NamagomiIgnore} from "./NamagomiIgnore";
+import path from "path";
 
+export function addMod(mod: FileList) {
+    if(!fs.existsSync(namagomiIgnore)) mkEmptyJson(namagomiIgnore)
+    const namagomiIgnoreJson = JSON.parse(fs.readFileSync(namagomiIgnore).toString()) as NamagomiIgnore
+
+    Array.from(mod)
+        .map(file => {
+            namagomiIgnoreJson.push(file.name)
+            fs.copyFileSync(file.path, path.join(modsDir, file.name))
+        })
+
+    fs.writeFileSync(namagomiIgnore, JSON.stringify(namagomiIgnoreJson))
+}
+
+function mkEmptyJson(path: string) {
+    fs.writeFileSync(path, JSON.stringify([]))
 }
