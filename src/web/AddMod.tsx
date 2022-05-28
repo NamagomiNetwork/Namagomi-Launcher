3c961dad63dbd701dc51618ca179aa99623101d9
yuuki1293 <yuuki1293@outlook.jp>
2022-05-28 21:26:27 +0900

    add: AddMod.tsx

---
+++ src/web/AddMod.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AddMod extends React.Component<{}, {}>{
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
