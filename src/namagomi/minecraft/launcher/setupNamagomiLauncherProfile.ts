12fbdf144f0b0c6e20a1684320ca2cca1bafe5b2
yuuki1293 <yuuki1293@outlook.jp>
2022-05-30 18:42:15 +0900

    fix: #26

--- src/namagomi/minecraft/launcher/setupNamagomiLauncherProfile.ts
+++ src/namagomi/minecraft/launcher/setupNamagomiLauncherProfile.ts
@@ -1,14 +1,13 @@
 import {LauncherProfileBuilder} from "./LauncherProfileBuilder";
-import {app} from 'electron';
-import path from "path";
+import {mainDir} from "../../settings/localPath";
 
 const namagomiBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAAAAADmVT4XAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABFSURBVHhe7c6hEQAgDATBQK2URbGYDC4ChciuefUzFwAAAAAAAABAeyP32rmVlVt5/c/cbwQIECBAgAABAAAAAABAdxEHuD0CIPKU7VAAAAAASUVORK5CYII="
 
 export function setup() {
     const launcher = new LauncherProfileBuilder()
     launcher
-        .set('uniqueId', 'namagomidev')
-        .set('gameDir', path.join(app.getPath('userData'),'minecraft\\dev'))
+        .set('uniqueId', 'namagomi')
+        .set('gameDir', mainDir)
         .set('icon', namagomiBase64)
         .set('name', '生ゴミ鯖')
         .set('lastVersionId', '1.12.2-forge-14.23.5.2860')
