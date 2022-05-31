424061f8be6dd23e25a77c26168e8ae0ae93b6ad
yuuki1293 <yuuki1293@outlook.jp>
2022-05-31 09:39:43 +0900

    update: GetMod

--- src/namagomi/minecraft/api/mods/CurseForgeAPIResponseTypes/GetMod.ts
+++ src/namagomi/minecraft/api/mods/CurseForgeAPIResponseTypes/GetMod.ts
@@ -1,130 +1,134 @@
-export type GetMod = {
-    "data"?: {
-        "id"?: number,
-        "gameId"?: number,
-        "name"?: string,
-        "slug"?: string,
-        "links"?: {
-            "websiteUrl"?: string,
-            "wikiUrl"?: string,
-            "issuesUrl"?: string,
-            "sourceUrl"?: string
-        },
-        "summary"?: string,
-        "status"?: number,
-        "downloadCount"?: number,
-        "isFeatured"?: boolean,
-        "primaryCategoryId"?: number,
-        "categories"?: [
-            {
-                "id"?: number,
-                "gameId"?: number,
-                "name"?: string,
-                "slug"?: string,
-                "url"?: string,
-                "iconUrl"?: string,
-                "dateModified"?: string,
-                "isClass"?: boolean,
-                "classId"?: number,
-                "parentCategoryId"?: number,
-                "displayIndex"?: number
-            }
-        ],
-        "classId"?: number,
-        "authors"?: [
-            {
-                "id"?: number,
-                "name"?: string,
-                "url"?: string
-            }
-        ],
-        "logo"?: {
-            "id"?: number,
-            "modId"?: number,
-            "title"?: string,
-            "description"?: string,
-            "thumbnailUrl"?: string,
-            "url"?: string
-        },
-        "screenshots"?: [
-            {
-                "id"?: number,
-                "modId"?: number,
-                "title"?: string,
-                "description"?: string,
-                "thumbnailUrl"?: string,
-                "url"?: string
-            }
-        ],
-        "mainFileId"?: number,
-        "latestFiles"?: [
-            {
-                "id"?: number,
-                "gameId"?: number,
-                "modId"?: number,
-                "isAvailable"?: boolean,
-                "displayName"?: string,
-                "fileName"?: string,
-                "releaseType"?: number,
-                "fileStatus"?: number,
-                "hashes"?: [
-                    {
-                        "value"?: string,
-                        "algo"?: number
-                    }
-                ],
-                "fileDate"?: string,
-                "fileLength"?: number,
-                "downloadCount"?: number,
-                "downloadUrl"?: string,
-                "gameVersions"?: [
-                    string
-                ],
-                "sortableGameVersions"?: [
-                    {
-                        "gameVersionName"?: string,
-                        "gameVersionPadded"?: string,
-                        "gameVersion"?: string,
-                        "gameVersionReleaseDate"?: string,
-                        "gameVersionTypeId"?: number
-                    }
-                ],
-                "dependencies"?: [
-                    {
-                        "modId"?: number,
-                        "relationType"?: number
-                    }
-                ],
-                "exposeAsAlternative"?: boolean,
-                "parentProjectFileId"?: number,
-                "alternateFileId"?: number,
-                "isServerPack"?: boolean,
-                "serverPackFileId"?: number,
-                "fileFingerprint"?: number,
-                "modules"?: [
-                    {
-                        "name"?: string,
-                        "fingerprint"?: number
-                    }
-                ]
-            }
-        ],
-        "latestFilesIndexes"?: [
-            {
-                "gameVersion"?: string,
-                "fileId"?: number,
-                "filename"?: string,
-                "releaseType"?: number,
-                "gameVersionTypeId"?: number,
-                "modLoader"?: number
-            }
-        ],
-        "dateCreated"?: string,
-        "dateModified"?: string,
-        "dateReleased"?: string,
-        "allowModDistribution"?: boolean,
-        "gamePopularityRank"?: number,
-        "isAvailable"?: boolean,
-        "thumbsUpCount"?: number
-    }
+export interface GetMod {
+    data: Data
+}
+
+interface Data {
+    id: number
+    gameId: number
+    name: string
+    slug: string
+    links: Links
+    summary: string
+    status: number
+    downloadCount: number
+    isFeatured: boolean
+    primaryCategoryId: number
+    categories: Category[]
+    classId: number
+    authors: Author[]
+    logo: Logo
+    screenshots: Screenshot[]
+    mainFileId: number
+    latestFiles: LatestFile[]
+    latestFilesIndexes: LatestFilesIndex[]
+    dateCreated: string
+    dateModified: string
+    dateReleased: string
+    allowModDistribution: boolean
+    gamePopularityRank: number
+    isAvailable: boolean
+    thumbsUpCount: number
+}
+
+interface Links {
+    websiteUrl: string
+    wikiUrl: string
+    issuesUrl: string
+    sourceUrl: string
+}
+
+interface Category {
+    id: number
+    gameId: number
+    name: string
+    slug: string
+    url: string
+    iconUrl: string
+    dateModified: string
+    isClass: boolean
+    classId: number
+    parentCategoryId: number
+    displayIndex: number
+}
+
+interface Author {
+    id: number
+    name: string
+    url: string
+}
+
+interface Logo {
+    id: number
+    modId: number
+    title: string
+    description: string
+    thumbnailUrl: string
+    url: string
+}
+
+interface Screenshot {
+    id: number
+    modId: number
+    title: string
+    description: string
+    thumbnailUrl: string
+    url: string
+}
+
+interface LatestFile {
+    id: number
+    gameId: number
+    modId: number
+    isAvailable: boolean
+    displayName: string
+    fileName: string
+    releaseType: number
+    fileStatus: number
+    hashes: Hash[]
+    fileDate: string
+    fileLength: number
+    downloadCount: number
+    downloadUrl: string
+    gameVersions: string[]
+    sortableGameVersions: SortableGameVersion[]
+    dependencies: Dependency[]
+    exposeAsAlternative: boolean
+    parentProjectFileId: number
+    alternateFileId: number
+    isServerPack: boolean
+    serverPackFileId: number
+    fileFingerprint: number
+    modules: Module[]
+}
+
+interface Hash {
+    value: string
+    algo: number
+}
+
+interface SortableGameVersion {
+    gameVersionName: string
+    gameVersionPadded: string
+    gameVersion: string
+    gameVersionReleaseDate: string
+    gameVersionTypeId: number
+}
+
+interface Dependency {
+    modId: number
+    relationType: number
+}
+
+interface Module {
+    name: string
+    fingerprint: number
+}
+
+interface LatestFilesIndex {
+    gameVersion: string
+    fileId: number
+    filename: string
+    releaseType: number
+    gameVersionTypeId: number
+    modLoader: number
 }
