a3f5fc1a6ff99a8b99fb91ff20f4367951782f53
yuuki1293 <yuuki1293@outlook.jp>
2022-06-19 20:30:53 +0900

    log に状態を追加

--- src/web/log/Log.tsx
+++ src/web/log/Log.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './Log.css'
 
 export const Log = () => {
+    const [log, setLog] = React.useState<string>('Logだよ～ん')
+
     return (
         <div className="log">
-            <p>Log</p>
+            <p>{log}</p>
         </div>
     )
 }
