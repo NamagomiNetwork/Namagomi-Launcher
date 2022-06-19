f4305ecd35ebc873e9ad54ae4259193cc91f19ff
yuuki1293 <yuuki1293@outlook.jp>
2022-06-19 18:07:25 +0900

    css 分からん

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './App.css'
 import {Version} from './Version'
 import {Switch} from './Switch'
+import {Log} from './log/Log'
 
 export const App = () => {
     return (
         <div className="container">
-            <Switch/>
-            <Version/>
+            <div className={"flexElements"}>
+                <Switch/>
+                <Log/>
+            </div>
+            <footer>
+                <Version/>
+            </footer>
         </div>
     )
 }
---
+++ src/web/log/Log.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Log.css'
+
+export const Log = () => {
+    return (
+        <div className="log">
+            <p>Log</p>
+        </div>
+    )
+}
