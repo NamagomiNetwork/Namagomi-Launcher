b312e488dda3808b39f39c350cf7b601e6f1b159
yuuki1293 <yuuki1293@outlook.jp>
2022-06-29 13:20:04 +0900

    add: LocalPaths

--- src/main/scala/com/github/namagomi/LocalPaths.scala
+++ src/main/scala/com/github/namagomi/LocalPaths.scala
@@ -1,5 +1,17 @@
 package com.github.namagomi
 
-class LocalPaths {
+import java.nio.file.Paths
 
+object LocalPaths {
+
+  import net.harawata.appdirs.{AppDirs, AppDirsFactory}
+
+  private val appDirs: AppDirs = AppDirsFactory.getInstance
+  val userDir: String = appDirs.getUserDataDir("Namagomi Launcher", null, "Namagomi Network")
+  val minecraftDir: String = Paths.get(userDir, "minecraft").toString
+  val mainDir: String => String = (side: String) => Paths.get(minecraftDir, s"main-$side").toString
+  val modsDir: String => String = (side: String) => Paths.get(mainDir(side), "mods").toString
+  val namagomiCache: String => String = (side: String) => Paths.get(mainDir(side), ".namagomi-cache").toString
+  val namagomiIgnore: String => String = (side: String) => Paths.get(mainDir(side), ".namagomi-cache").toString
+  val logsDir: String = Paths.get(userDir, "logs").toString
 }
