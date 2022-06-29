69ecbe2538ff238c83d34d3186162c49067a11f6
yuuki1293 <yuuki1293@outlook.jp>
2022-06-29 15:17:20 +0900

    update: getModList

--- src/main/scala/com/github/namagomi/github/Github.scala
+++ src/main/scala/com/github/namagomi/github/Github.scala
@@ -1,15 +1,41 @@
 package com.github.namagomi.github
 
+import com.github.namagomi._
 import io.circe.Decoder
 import io.circe.generic.semiauto.deriveDecoder
-import sttp.client3.{HttpClientSyncBackend, Identity, SttpBackend}
+import sttp.client3._
+import sttp.client3.circe._
 
 object Github {
   private val backend: SttpBackend[Identity, Any] = HttpClientSyncBackend()
 
-  private implicit val decoder: Decoder[] = deriveDecoder
+  private implicit val decoder: Decoder[List[NamagomiModResponse]] = deriveDecoder
 
-  def getModList(url: String) = {
+  def getModList(url: String): Either[ResponseException[String, io.circe.Error], List[NamagomiModData]] = {
+    val response = basicRequest
+      .get(uri"$url")
+      .response(asJson[List[NamagomiModResponse]])
+      .send(backend)
 
+    response.body match {
+      case Right(value) =>
+        Right(value.map(i =>
+          (i.directUrl, i.modId, i.fileId) match {
+            case (Some(directUrl), _, _) =>
+              HasDownloadUrl(
+                i.name,
+                directUrl,
+                i.side
+              )
+            case (_, Some(modId), Some(fileId)) =>
+              HasNotDownloadUrl(
+                i.name,
+                modId,
+                fileId,
+                i.side
+              )
+          }))
+      case Left(e) => Left(e)
+    }
   }
 }
