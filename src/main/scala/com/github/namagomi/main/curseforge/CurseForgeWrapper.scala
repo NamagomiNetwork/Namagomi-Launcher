3fabed7b16c9448af6f563d4cf8bfd48ff4470ac
yuuki1293 <yuuki1293@outlook.jp>
2022-07-01 09:12:54 +0900

    update: auto decoder

--- src/main/scala/com/github/namagomi/main/curseforge/CurseForgeWrapper.scala
+++ src/main/scala/com/github/namagomi/main/curseforge/CurseForgeWrapper.scala
@@ -4,8 +4,7 @@ import com.github.namagomi.main.Config.{curseForgeApiKey, curseForgeUrl, namagom
 import com.github.namagomi.main.LocalPaths._
 import com.github.namagomi.main.github.Github.getModList
 import com.github.namagomi.main.{HasDownloadUrl, HasNotDownloadUrl, NamagomiModData, Unexpected}
-import io.circe.Decoder
-import io.circe.generic.semiauto._
+import io.circe.generic.auto._
 import sttp.client3._
 import sttp.client3.circe._
 import sttp.model.Uri
@@ -20,41 +19,32 @@ object CurseForgeWrapper {
 
   private val backend: SttpBackend[Identity, Any] = HttpClientSyncBackend()
 
-  private implicit val decodeHash: Decoder[Hash] = deriveDecoder
-  private implicit val decodeSortableGameVersion: Decoder[SortableGameVersion] = deriveDecoder
-  private implicit val decodeModule: Decoder[Module] = deriveDecoder
-  private implicit val decoder: Decoder[CurseForgeResponse] = deriveDecoder
-
-  private def getModFileUrl(namagomiMod: NamagomiModData): Either[ResponseException[String, io.circe.Error], NamagomiModResponse] = {
+  private def getModFileUrl(namagomiMod: NamagomiModData): NamagomiModResponse = {
     namagomiMod match {
       case HasDownloadUrl(_, directUrl, side) =>
-        Right(NamagomiModResponse(
+        NamagomiModResponse(
           side,
           getFileName(directUrl),
           Some(directUrl),
           None,
-        ))
+        )
       case HasNotDownloadUrl(_, modId, fileId, side) =>
         val getFileUrl = Uri(curseForgeUrl, Seq("v1/mods", modId, "files", fileId))
         val response = basicRequest
           .headers(curseForgeHeaders)
           .post(getFileUrl)
-          .response(asJson[CurseForgeResponse])
+          .response(asJson[CurseForgeResponse].getRight)
           .send(backend)
 
-        response.body match {
-          case Right(value) =>
-            Right(NamagomiModResponse(
-              side,
-              value.fileName,
-              value.downloadUrl,
-              Some(value.hashes)
-            ))
-          case Left(value) =>
-            Left(value)
-        }
+        NamagomiModResponse(
+          side,
+          response.body.fileName,
+          response.body.downloadUrl,
+          Some(response.body.hashes)
+        )
     }
   }
+
 
   private def getFileName(url: String): String = {
     url match {
@@ -76,33 +66,33 @@ object CurseForgeWrapper {
           .body match {
           case Left(value) =>
             println(s"[ERROR] $value")
+          case Right(value) =>
+            println(s"[INFO] downloaded: ${
+              value.getName
+            }")
         }
       case _ =>
-        println(s"${namagomiMod.fileName} hasn't download url")
+        println(s"${
+          namagomiMod.fileName
+        } hasn't download url")
     }
   }
 
   def downloadModFiles(side: String): Unit = {
     val modList = getModList(namagomiModListUrl("main"))
 
-    modList match {
-      case Right(modList) =>
-        modList.foreach {
-          case Unexpected(name, modId, fileId, directUrl, side) =>
-            println("Unexpected json value")
-            println(s"\tname: $name")
-            println(s"\tmodId: $modId")
-            println(s"\tfileId: $fileId")
-            println(s"\tdirectUrl: $directUrl")
-            println(s"\tside: $side")
-          case mod =>
-            getModFileUrl(mod) match {
-              case Right(url) if url.side.contains(side) =>
-                downloadModFile(url, side)
-              case Left(e) => println(e)
-            }
-        }
-      case Left(e) => println(e)
+    modList.foreach {
+      case Unexpected(name, modId, fileId, directUrl, side) =>
+        println("Unexpected json value")
+        println(s"\tname: $name")
+        println(s"\tmodId: $modId")
+        println(s"\tfileId: $fileId")
+        println(s"\tdirectUrl: $directUrl")
+        println(s"\tside: $side")
+      case mod =>
+        val url = getModFileUrl(mod)
+        if (getModFileUrl(mod).side.contains(side))
+          downloadModFile(url, side)
     }
   }
 }
