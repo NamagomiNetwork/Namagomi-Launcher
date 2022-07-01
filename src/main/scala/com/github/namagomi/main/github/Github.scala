31c59a79025834c3f2b496d7ce0aaba7778f7de3
yuuki1293 <yuuki1293@outlook.jp>
2022-07-01 09:13:02 +0900

    update: auto decoder

--- src/main/scala/com/github/namagomi/main/github/Github.scala
+++ src/main/scala/com/github/namagomi/main/github/Github.scala
@@ -1,52 +1,42 @@
 package com.github.namagomi.main.github
 
-import com.github.namagomi.main.curseforge.Hash
 import com.github.namagomi.main.{HasDownloadUrl, HasNotDownloadUrl, NamagomiModData, Unexpected}
-import io.circe.Decoder
-import io.circe.generic.semiauto.deriveDecoder
+import io.circe.generic.auto._
 import sttp.client3._
 import sttp.client3.circe._
 
 object Github {
   private val backend: SttpBackend[Identity, Any] = HttpClientSyncBackend()
 
-  private implicit val decodeHash: Decoder[Hash] = deriveDecoder
-  private implicit val decodeNamagomiModResponse: Decoder[NamagomiModResponse] = deriveDecoder
-  private implicit val decoder: Decoder[List[NamagomiModResponse]] = deriveDecoder
-
-  def getModList(url: String): Either[ResponseException[String, io.circe.Error], List[NamagomiModData]] = {
+  def getModList(url: String): List[NamagomiModData] = {
     val response = basicRequest
       .get(uri"$url")
-      .response(asJson[List[NamagomiModResponse]])
+      .response(asJson[List[NamagomiModResponse]].getRight)
       .send(backend)
 
-    response.body match {
-      case Right(value) =>
-        Right(value.map(i =>
-          (i.directUrl, i.modId, i.fileId) match {
-            case (Some(directUrl), _, _) =>
-              HasDownloadUrl(
-                i.name,
-                directUrl,
-                i.side
-              )
-            case (_, Some(modId), Some(fileId)) =>
-              HasNotDownloadUrl(
-                i.name,
-                modId,
-                fileId,
-                i.side
-              )
-            case _ =>
-              Unexpected(
-                i.name,
-                i.modId,
-                i.fileId,
-                i.directUrl,
-                i.side
-              )
-          }))
-      case Left(e) => Left(e)
-    }
+    response.body.map(i =>
+      (i.directUrl, i.modId, i.fileId) match {
+        case (Some(directUrl), _, _) =>
+          HasDownloadUrl(
+            i.name,
+            directUrl,
+            i.side
+          )
+        case (_, Some(modId), Some(fileId)) =>
+          HasNotDownloadUrl(
+            i.name,
+            modId,
+            fileId,
+            i.side
+          )
+        case _ =>
+          Unexpected(
+            i.name,
+            i.modId,
+            i.fileId,
+            i.directUrl,
+            i.side
+          )
+      })
   }
 }
