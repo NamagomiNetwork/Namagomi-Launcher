bb80407151959755d036ebd4924f2771817269c6
yuuki1293 <yuuki1293@outlook.jp>
2022-07-03 23:18:55 +0900

    update: NamagomiModResponseProtocol

--- src/main/scala/com/github/namagomi/main/github/NamagomiModResponseProtocol.scala
+++ src/main/scala/com/github/namagomi/main/github/NamagomiModResponseProtocol.scala
@@ -1,5 +1,7 @@
 package com.github.namagomi.main.github
 
-object NamagomiModResponseProtocol {
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
+object NamagomiModResponseProtocol extends DefaultJsonProtocol {
+  implicit val namagomiModResponseProtocol: RootJsonFormat[NamagomiModResponse] = jsonFormat(NamagomiModResponse, "name", "modId", "fileId", "directUrl", "mcVersion", "side")
 }
