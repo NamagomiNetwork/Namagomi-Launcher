689344599e71281ea26447a5d85b0dd8150dd51d
yuuki1293 <yuuki1293@outlook.jp>
2022-07-05 09:57:02 +0900

    update: Protocol

--- src/main/scala/com/github/namagomi/main/github/Protocol.scala
+++ src/main/scala/com/github/namagomi/main/github/Protocol.scala
@@ -1,5 +1,8 @@
 package com.github.namagomi.main.github
 
-object Protocol {
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
+object Protocol extends DefaultJsonProtocol {
+  implicit val treeProtocol: RootJsonFormat[Tree] = jsonFormat(Tree.apply, "path", "mode", "type", "sha", "url")
+  implicit val gitTreesResponseProtocol: RootJsonFormat[GitTreesResponse] = jsonFormat(GitTreesResponse.apply, "sha", "url", "tree")
 }
