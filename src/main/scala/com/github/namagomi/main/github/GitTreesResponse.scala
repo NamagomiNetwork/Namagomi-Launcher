f099b5f6e91ac8ea630fa1d431d70f5bff2222b7
yuuki1293 <yuuki1293@outlook.jp>
2022-07-05 09:51:13 +0900

    update: GitTreesResponse

--- src/main/scala/com/github/namagomi/main/github/GitTreesResponse.scala
+++ src/main/scala/com/github/namagomi/main/github/GitTreesResponse.scala
@@ -1,5 +1,17 @@
 package com.github.namagomi.main.github
 
-class GitTreesResponse {
+case class GitTreesResponse
+(
+  sha: String,
+  url: String,
+  tree: Tree
+)
 
-}
+case class Tree
+(
+  path: String,
+  mode: String,
+  `type`: String,
+  sha: String,
+  url: String,
+)
