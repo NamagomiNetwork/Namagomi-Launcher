548c3e27cab4bd0f0f26ffb48cda9d02728d84ca
yuuki1293 <yuuki1293@outlook.jp>
2022-07-11 09:22:25 +0900

    update: NamagomiCacheProtocol

--- src/main/scala/com/github/namagomi/main/data/NamagomiCacheProtocol.scala
+++ src/main/scala/com/github/namagomi/main/data/NamagomiCacheProtocol.scala
@@ -1,5 +1,8 @@
 package com.github.namagomi.main.data
 
-object NamagomiCacheProtocol {
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
+object NamagomiCacheProtocol extends DefaultJsonProtocol {
+  implicit val dataProtocol: RootJsonFormat[Data] = jsonFormat(Data.apply, "name", "sha")
+  implicit val namagomiCacheProtocol: RootJsonFormat[NamagomiCache] = jsonFormat(NamagomiCache.apply, "data", "mods")
 }
