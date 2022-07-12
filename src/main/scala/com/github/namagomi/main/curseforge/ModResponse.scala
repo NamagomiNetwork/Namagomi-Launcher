bcfdb7062c4ed81468942676b4e21748a58ebcf9
yuuki1293 <yuuki1293@outlook.jp>
2022-07-12 09:15:22 +0900

    update: ModResponse

--- src/main/scala/com/github/namagomi/main/curseforge/ModResponse.scala
+++ src/main/scala/com/github/namagomi/main/curseforge/ModResponse.scala
@@ -1,3 +1,151 @@
 package com.github.namagomi.main.curseforge
 
-case class ModResponse()
+object ModResponse {
+  case class ModResponse
+  (
+    data: Data
+  )
+
+  case class Data
+  (
+    id: Int,
+    gameId: Int,
+    name: String,
+    slug: String,
+    links: Links,
+    summary: String,
+    status: Int,
+    downloadCount: Int,
+    isFeatured: Boolean,
+    primaryCategoryId: Int,
+    categories: Seq[Category],
+    classId: Int,
+    authors: Seq[Author],
+    logo: Logo,
+    screenshots: Seq[Screenshot],
+    mainFileId: Int,
+    latestFiles: Seq[LatestFile],
+    latestFilesIndexes: Seq[LatestFilesIndex],
+    dateCreated: String,
+    dateModified: String,
+    dateReleased: String,
+    allowModDiStribution: Boolean,
+    gamePopularityRank: Int,
+    isAvailable: Boolean,
+    thumbsUpCount: Int,
+  )
+
+  case class Links
+  (
+    websiteUrl: String,
+    wikiUrl: String,
+    issuesUrl: String,
+    sourceUrl: String,
+  )
+
+  case class Category
+  (
+    id: Int,
+    gameId: Int,
+    name: String,
+    slug: String,
+    url: String,
+    iconUrl: String,
+    dateModified: String,
+    isClass: Boolean,
+    classId: Int,
+    parentCategoryId: Int,
+    displayIndex: Int,
+  )
+
+  case class Author
+  (
+    id: Int,
+    name: String,
+    url: String
+  )
+
+  case class Logo
+  (
+    id: Int,
+    modId: Int,
+    title: String,
+    description: String,
+    thumbnailUrl: String,
+    url: String
+  )
+
+  case class Screenshot
+  (
+    id: Int,
+    modId: Int,
+    title: String,
+    description: String,
+    thumbnailUrl: String,
+    url: String
+  )
+
+  case class LatestFile
+  (
+    id: Int,
+    gameId: Int,
+    modId: Int,
+    isAvailable: Boolean,
+    displayName: String,
+    fileName: String,
+    releaseType: Int,
+    fileStatus: Int,
+    hashes: Seq[Hash],
+    fileDate: String,
+    fileLength: Int,
+    downloadCount: Int,
+    downloadUrl: String,
+    gameVersions: Seq[String],
+    sortableGameVersions: Seq[SortableGameVersion],
+    dependencies: Seq[Dependency],
+    exposeAsAlternative: Boolean,
+    parentProjectFileId: Int,
+    alternateFileId: Int,
+    isServerPack: Boolean,
+    serverPackFileId: Int,
+    fileFingerprint: Int,
+    modules: Seq[Module]
+  )
+
+  case class Hash
+  (
+    value: String,
+    algo: Int
+  )
+
+  case class SortableGameVersion
+  (
+    gameVersionName: String,
+    gameVersionPadded: String,
+    gameVersion: String,
+    gameVersionReleaseDate: String,
+    gameVersionTypeId: Int
+  )
+
+  case class Dependency
+  (
+    modId: Int,
+    relationType: Int
+  )
+
+  case class Module
+  (
+    name: String,
+    fingerprint: Long
+  )
+
+  case class LatestFilesIndex
+  (
+    gameVersion: String,
+    fileId: Int,
+    filename: String,
+    releaseType: Int,
+    gameVersionTypeId: Int,
+    modLoader: Int,
+  )
+}
